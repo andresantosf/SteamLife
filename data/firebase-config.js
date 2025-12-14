78f91381eb7f44edf6dfdb8b19aa6924ce354db3
andre.santos <andre.santos@pdcase.com.br>
2025-12-14 12:42:35 -0300

    Move prepare-public before deploy

---
+++ data/firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+    apiKey: "AIzaSyCZrzh3BEGX_vktSEFd2OGtaCM3AcEjqDA",
+    authDomain: "steam-life-f064f.firebaseapp.com",
+    projectId: "steam-life-f064f",
+    storageBucket: "steam-life-f064f.firebasestorage.app",
+    messagingSenderId: "141116654680",
+    appId: "1:141116654680:web:bcde6ddcf688aa2a76f62d",
+    measurementId: "G-4YEQ8757KV"
+};
---
+++ ls
@@ -0,0 +1,2 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+     6300       1    6300       6300  cons0     197609 12:40:27 /usr/bin/ps
